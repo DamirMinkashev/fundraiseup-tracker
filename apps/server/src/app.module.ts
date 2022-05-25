db22205fd7265f78fbf7cff68b2f65139b2189a5
Damir Minkashev <minkashev@trueconf.ru>
2022-05-25 19:53:09 +0300

    add getting mongo url from .env

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TrackerModule } from './tracker/tracker.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
 	imports: [
 		TrackerModule,
+		ConfigModule.forRoot(),
 		MongooseModule.forRootAsync({
-			useFactory: () => ({
-				uri: 'mongodb://127.0.0.1:27017/tracker',
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (config: ConfigService) => ({
+				uri:
+					config.get('MONGODB_URI') ??
+					'mongodb://127.0.0.1:27017/tracker',
 			}),
 		}),
 	],
